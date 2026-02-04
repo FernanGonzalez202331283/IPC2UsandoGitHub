c493af3d9a8f68ed06f3b85e0f4491b3f04e1ef6
Fernan Gonzalez <fernangonzalez202331283@cunoc.edu.gt>
2026-02-03 20:05:27 -0600

    subiendo archivo

---
+++ MiPrimerGitHub/src/main/java/com/mycompany/miprimergithub/MiPrimerGitHub.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.miprimergithub;
+
+/**
+ *
+ * @author fernan
+ */
+public class MiPrimerGitHub {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
